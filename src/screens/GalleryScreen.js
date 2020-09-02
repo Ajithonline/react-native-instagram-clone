00a41c6da5ccec4d6546f513f6cc7e69bb6671d7
ajith <ajithvonline@gmail.com>
2020-09-02 10:19:57 +0530

    gallery part 15

--- src/screens/GalleryScreen.js
+++ src/screens/GalleryScreen.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Text, View,StyleSheet, ScrollView,PermissionsAndroid,Platform,Dimensions,Image } from 'react-native'
+import { Text, View,StyleSheet, ScrollView,PermissionsAndroid,Platform,Dimensions,Image,TouchableOpacity } from 'react-native'
 import { colors } from '../config/Colors'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import CameraRoll from "@react-native-community/cameraroll";
+import {Picker} from '@react-native-community/picker';
 
 const width=Dimensions.get('window').width
 const height=Dimensions.get('window').height
@@ -27,25 +28,59 @@ export class GalleryScreen extends Component {
     constructor(props){
         super(props)
         this.state={images:[],
-                    pickedImage:''}
+                    albums:[],
+                    pickedImage:'',
+                    category: '',}
+        this.imgArr=[]            
     }
 
     async componentDidMount(){
         if (Platform.OS === "android" && !(await hasAndroidPermission())) {
             return;
           }
-        let params={first:20}
-          CameraRoll.getPhotos(params).then((images)=>{
-           
-              this.setState({images:images.edges,pickedImage:images.edges[0].node.image.uri})
-          }).catch((error)=>{
-              console.log(error)
-          });
+        
+          let params={assetType:'All'}
+          CameraRoll.getAlbums(params).then((albums)=>{
+             this.setState({albums:albums})
+             console.log(albums)
+          }).catch((err)=>{
+              console.error(err)
+          }); 
     }
 
+    imagePressed=(item,key)=>{
+     this.setState({pickedImage:item.node.image.uri})
+    this.scrollview_ref.scrollTo({
+        x:0,
+        y:this.imgArr[key],
+        Animation:true
+    })
+     } 
+     pickedCategory=(itemValue,itemIndex)=>{
+         let params={first:40,
+                     groupName:this.state.albums[itemIndex].title}
+
+        this.setState({category:itemValue})
+
+         CameraRoll.getPhotos(params).then((imgs)=>{
+            console.log(params)
+             console.log(imgs)
+             this.setState({images:imgs.edges,pickedImage:imgs.edges[0].node.image.uri})
+         });
+     }
     displayImages=()=>{
-        return this.state.images.map((item)=>{
-            return(<View><Image style={styles.image} source={{uri:item.node.image.uri}} /></View>)
+        return this.state.images.map((item,key)=>{
+            return(<TouchableOpacity key={key} onLayout={(event)=>{
+                  const layout=event.nativeEvent.layout;
+                  this.imgArr[key]=layout.y}} 
+                  key={key} onPress={()=>{this.imagePressed(item,key)}}>
+                     <Image style={styles.image} source={{uri:item.node.image.uri}} />
+                  </TouchableOpacity>)
+        })
+    }
+    displayAssetCategories=()=>{
+        return this.state.albums.map((category,index)=>{
+            return <Picker.Item label={category.title} value={index} />
         })
     }
     render() {
@@ -54,7 +89,17 @@ export class GalleryScreen extends Component {
                 <View style={styles.headerWrapper}>
                    <View style={styles.headerLeftWrapper}>
                       <View><Icon size={25} name="times"  /></View>
-                      <View style={styles.headerTitleWrapper}><Text><Text style={styles.headerTitle}>Gallery</Text> <Icon name="caret-down"  /></Text></View>
+                      <View style={styles.headerTitleWrapper}>
+                          <Picker
+                          mode={'dropdown'}
+                          selectedValue={this.state.category}
+                          style={{height: 30, width: width/2}}
+                          onValueChange={(itemValue, itemIndex) =>
+                           this.pickedCategory(itemValue,itemIndex) }>
+                           {this.displayAssetCategories()}
+                         </Picker>
+                        {/*   <Text><Text style={styles.headerTitle}>Gallery</Text> <Icon name="caret-down"  /></Text> */}
+                      </View>
                    </View>
                    <View>
                       <View><Text style={styles.headerSubTitle}>Next</Text></View>
@@ -63,7 +108,7 @@ export class GalleryScreen extends Component {
                 <View>
                  <Image style={styles.pickedImageWrapper} source={{uri:this.state.pickedImage}} />
                 </View>
-                <ScrollView>
+                <ScrollView ref={ref=>{this.scrollview_ref=ref}}>
                    <View style={styles.galleryImagesWrapper}> 
                        {this.displayImages()}
                    </View>
@@ -134,8 +179,9 @@ export const styles=StyleSheet.create({
         color:colors.primary
     },
     image:{
-        width:width/4,
+        width:width/4.1,
         height:width/4,
+        padding:1
       
     },
     galleryImagesWrapper:{
@@ -144,6 +190,6 @@ export const styles=StyleSheet.create({
         flexWrap:'wrap'
     },
     pickedImageWrapper:{
-        height:height/3
+        height:height/2
     }
 })
